b4c28053d3edccfb5ac441b70cddc341aea79687
Tung Bui <tung.bquang@gmail.com>
2024-03-13 22:29:14 +0700

    Update deploy

--- aks-mysql/deploy.sh
+++ aks-mysql/deploy.sh
@@ -2,9 +2,18 @@
 
 # Deploy AKS fresh cluster first
 
+# Create an SA password
+kubectl create secret generic mssql --from-literal=MSSQL_SA_PASSWORD="MyC0m9l&xP@ssw0rd"
 
 # Create storage
 kubectl apply -f manifests/mysql-pvc.yaml
 
-# Verify
+# Verify PVC
 kubectl describe pvc mssql-data
+
+# Deploy SQL
+kubectl apply -f manifests/mysql-deployment.yaml
+
+# Verify
+kubectl get pods
+kubectl get services
