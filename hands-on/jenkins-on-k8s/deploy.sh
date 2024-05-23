a8081dd5a90501c8fbe8901eb56a38f59cf6be6d
Tung Bui <tung.bquang@gmail.com>
2024-05-23 07:07:12 +0700

    Init jenkins on k8s.v1

--- hands-on/jenkins-on-k8s/deploy.sh
+++ hands-on/jenkins-on-k8s/deploy.sh
@@ -7,5 +7,5 @@ kubectl apply -f yamls/volumne.yaml
 kubectl apply -f yamls/deployment.yaml
 kubectl get deployments -n devops-tools
 
-kubectl apply -f service.yaml
+kubectl apply -f yamls/services.yaml
 kubectl get services -n devops-tools
